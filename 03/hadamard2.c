d5efcc08f8c31161bdc8c163523bda12c797958d
Jonas Wagner <jonas.a.wagner@outlook.de>
2020-03-30 15:22:21 +0200

    updated everything

---
+++ 03/hadamard2.c
@@ -0,0 +1,43 @@
+//Peter Koll, Jonas Wagner
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+#include <time.h>
+
+int SIZE = 10000;
+
+int main(){
+  double start, end;
+  int n_threads = 1;
+
+  int **a = (int**)malloc(SIZE * sizeof(int*));
+  int **b = (int**)malloc(SIZE * sizeof(int*));
+  int **c = (int**)malloc(SIZE * sizeof(int*));
+
+  for(int i = 0; i < SIZE; i++){
+    a[i] = (int*)malloc(SIZE * sizeof(int));
+    b[i] = (int*)malloc(SIZE * sizeof(int));
+    c[i] = (int*)malloc(SIZE * sizeof(int));
+  }
+
+
+  srand(time(NULL));
+  for(int i = 0; i < SIZE; i++){
+    for(int j = 0; j < SIZE; j++){
+      a[i][j] = rand();
+      b[i][j] = rand();
+    }
+  }
+
+  start = omp_get_wtime();
+  #pragma omp parallel for collapse(2) num_threads(n_threads)
+    for (int j = 0; j < SIZE; ++j) {
+      for (int i = 0; i < SIZE; ++i) {
+          c[i][j] = a[i][j] * b[i][j];
+      }
+    }
+  end = omp_get_wtime();
+  printf("Work took %f seconds (%d threads)\n", end - start, n_threads);
+
+  return EXIT_SUCCESS;
+}
---
+++ 03/job_03_02_had2.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Executes job in the queue "std.q" unless you have special requirements.
+#$ -q std.q
+
+# Changes to the current working directory before performing any further action
+#$ -cwd
+
+# Name of your job. Unless you use the -o and -e options, output will
+# go to a unique file name.ojob_id for each job.
+#$ -N had2
+
+# Redirect output stream to this file.
+#$ -o output_had1.dat
+
+# Join the error stream to the output stream.
+#$ -j yes
+
+# Parallel environment for using OpenMP, allocates 8 cores on a single node
+#$ -pe openmp 8
+
+# Use gcc 8.2.0 as the default gcc
+#module load gcc/8.2.0
+
+# Set up any environment variables
+#export ENVIRONMENT_VARIABLE=foobar
+./had2
